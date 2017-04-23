9242aa81875b52b2b967581a835ca8b84c777333
x <x@xs-MacBook-Air.local>
2017-04-23 07:28:17 -0500

    another update

--- wp-content/themes/zerif-lite/js/custom.js
+++ wp-content/themes/zerif-lite/js/custom.js
@@ -2,9 +2,10 @@ jQuery(document).ready(function($){
 	$("#menu-primary li a").each(function(){
       if($(this).attr('href') == '#'){
       	$(this).css({'cursor':'default','text-decoration':'none'});
-        $(this).bind('click',function(e){
-        		e.preventDefault();
-        });
+		$(this).removeAttr('href');
+        //$(this).bind('click',function(e){
+        //		e.preventDefault();
+        //});
       }
   });
 });
