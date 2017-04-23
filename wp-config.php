9c6fe660751812e2f90d008ee6cb8b2749b3a112
Aayush Tuladhar <aayush.tuladhar@target.com>
2017-04-22 19:28:04 -0500

    Pushing Remote DB

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'owc2017');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'test');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'Clock124');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '198.199.91.236');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
